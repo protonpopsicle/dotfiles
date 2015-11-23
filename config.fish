38a99af90210d5e687ee16fb48bbdd1f7a029f65
Scott Meisburger <smeisburger@gmail.com>
2015-11-23 10:38:16 -0500

    abc

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 set -x PATH /usr/local/sbin $PATH
 set -x PYTHONDONTWRITEBYTECODE True
 set -x EDITOR 'emacs -nw'
-set -x LEDGER_TERM 'qt persist'
+set -x LEDGER_TERM 'aqua'
 
 set -x API_CONFIG ~/team15/api/local_settings.py
 set -x WEBAPP_CONFIG ~/team15/webapp/local_settings.py
