e5a1a2f791646fb22b2c960ea84caef27ca9110f
Scott Meisburger <smeisburger@gmail.com>
2016-03-11 18:01:16 -0500

    yaya

--- bar_feeder.sh
+++ bar_feeder.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 clock() {
-    echo "%{F#FF0}%{B#00F}"
     date '+%-l:%M %p'
 }
 
@@ -10,7 +9,7 @@ battery() {
     capacity=`cat $dir/capacity`
     satus=`cat $dir/status`
 
-    test "$status" = "Full" && echo -n '+' || echo -n '-'
+    #test "$status" = "Full" && echo -n '+' || echo -n '-'
     echo $capacity%
 }
 
@@ -19,18 +18,36 @@ network() {
         echo "up" || echo "down"
 }
 
+meter() {
+    local v=$1
+    local max=$2
+    local color1=$3
+    local color2=$4
+    local diff=`expr $max - $v`
+    local fill1=""
+    local fill2=""
+
+    if [ $v != 0 ]; then
+        fill1=`printf "%0.s " $(seq 1 $v)`
+    fi
+    if [ $diff != 0 ]; then
+	fill2=`printf "%0.s " $(seq 1 $diff)`
+    fi
+    echo "$color1$fill1$color2$fill2"    
+}
+
 backlight() {
     local dir=/sys/class/backlight/acpi_video0
     brightness=`cat $dir/brightness`
     max_brightness=`cat $dir/max_brightness`
-    echo "%{B#000}%{F#0F0}"
-    printf "%0.s-" $(seq 1 $brightness)
-    echo "%{F#070}"
-    printf "%0.s-" $(seq 1 $max_brightness - $brightness)
+    meter $brightness $max_brightness "%{B#00F}" "%{B#007}"
 }
 
 while :; do
     end="%{F-}%{B-}"
-    echo "${l} `brightness` ${r} `clock` `battery` `network` $end"
+    c1="%{F#FF0}%{B#00F}"
+    c2="%{F#FFF}%{B#777}"
+    c3="%{F#F00}%{B#000}"
+    echo "%{l}`backlight`%{r}$c1 `clock` $c2 `battery` $c3 `network`$end"
     sleep 1
 done
