3af1d8fc08ffd0b0dc50fca8b502d6f208e58f58
Scott Meisburger <smeisburger@gmail.com>
2016-03-18 13:30:47 -0400

    lyihwef

--- bar_feeder.sh
+++ bar_feeder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 clock() {
-    date '+%a %-l:%M %p'
+    date '+%a %b %-e  %-l:%M %p'
 }
 
 battery() {
@@ -10,41 +10,39 @@ battery() {
     local satus=`cat $dir/status`
 
     #test "$status" = "Full" && echo -n '+' || echo -n '-'
-    echo $capacity%
+    meter $capacity, 100
 }
 
 network() {
     ping -c 1 8.8.8.8 >/dev/null 2>&1 && 
-        echo "  " || echo "!!"
+        echo " " || echo "!"
 }
 
 meter() {
     local v=$1
     local max=$2
-    local color1=$3
-    local color2=$4
     local diff=`expr $max - $v`
-    local fill1=""
-    local fill2=""
+    local fill1
+    local fill2
 
     if [ $v != 0 ]; then
-        fill1=`printf "%0.s " $(seq 1 $v)`
+        fill1=`printf "%0.s|" $(seq 1 $v)`
     fi
     if [ $diff != 0 ]; then
 	fill2=`printf "%0.s " $(seq 1 $diff)`
     fi
-    echo "$color1$fill1$color2$fill2"    
+    echo "[$fill1$fill2]"
 }
 
 backlight() {
     local dir=/sys/class/backlight/acpi_video0
     brightness=`cat $dir/brightness`
     max_brightness=`cat $dir/max_brightness`
-    meter $brightness $max_brightness "%{B#00F}" "%{B#007}"
+    meter $brightness $max_brightness
 }
 
 while :; do
     # c0="%{F-}%{B-}" # resets color to default
-    echo "%{r}`network`  `battery`  `clock` "
+    echo "%{r}`battery`   `clock` "
     sleep 1
 done
