43878719828517c00da018c03aaf2101228ed247
Scott Meisburger <smeisburger@gmail.com>
2018-03-22 16:01:18 -0400

    updated my journal

--- my-loadpackages.el
+++ my-loadpackages.el
@@ -14,7 +14,7 @@
 
 (with-eval-after-load "package"
   (add-to-list 'package-archives
-	       '("melpa" . "http://melpa.milkbox.net/packages/") t)
+	       '("melpa" . "http://melpa.org/packages") t)
   (add-to-list 'package-archives
 	       '("marmalade" . "http://marmalade-repo.org/packages/") t)
   )
